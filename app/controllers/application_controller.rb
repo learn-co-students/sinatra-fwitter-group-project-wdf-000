62394eb9861667d5b20a2732c734b950071622c3
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-18 17:09:01 -0400

    finished building functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,13 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
+    enable :sessions
+    set :session_secret, "qwertyuiop"
     set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :'../home'
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   def slug
     username.downcase.gsub(' ', '-')
   end
+  def self.find_by_slug(slug)
+    all.detect {|user| user.slug == slug}
+  end
 end
