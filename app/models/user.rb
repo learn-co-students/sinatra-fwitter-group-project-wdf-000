d5abc5c1ae2c066c2c44f8c19c930d7384b3abcd
ozPop <ozziepopovas@gmail.com>
2016-08-15 14:45:35 -0400

    add module slugifiable, update model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
   has_many :tweets
   validates_presence_of :username, :email, :password
   has_secure_password
