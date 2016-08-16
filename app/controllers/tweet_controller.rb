228e97e3664bf2a90ebda860bb644403e0ea1eee
ozPop <ozziepopovas@gmail.com>
2016-08-16 08:25:01 -0400

    add create new tweet view, add related actions/routes

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -10,4 +10,22 @@ class TweetController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    if logged_in?
+      @user = current_user(session)
+      erb :'/tweets/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    tweet = Tweet.create(content: params[:content], user_id: session[:user_id])
+    if tweet.save
+      redirect '/tweets'
+    else
+      redirect '/tweets/new'
+    end
+  end
+
 end
