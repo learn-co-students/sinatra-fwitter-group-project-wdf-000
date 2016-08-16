950468b75c3fb3224a3b4baa743a87a076ed989c
ozPop <ozziepopovas@gmail.com>
2016-08-16 07:48:31 -0400

    move routes and actions, add route to user controller
    * moved signup and login actions/routes to application controller
    * add /users/:slug route to user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,53 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    # binding.pry
     erb :'/application/index'
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'/registration/signup'
+    end
+  end
+
+  post '/signup' do
+    user = User.create(params)
+    if user.save
+      # binding.pry
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'/application/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
   end
 
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,50 +1,10 @@
 class UserController < ApplicationController
   include Helpers::InstanceMethods
 
-  get '/signup' do
-    if logged_in?
-      redirect '/tweets'
-    else
-      erb :'/registration/signup'
-    end
-  end
-
-  post '/signup' do
-    user = User.create(params)
-    if user.save
-      # binding.pry
-      session[:user_id] = user.id
-      redirect '/tweets'
-    else
-      redirect '/signup'
-    end
-  end
-
-  get '/login' do
-    if logged_in?
-      redirect '/tweets'
-    else
-      erb :'/application/login'
-    end
-  end
-
-  post '/login' do
-    user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect '/tweets'
-    else
-      redirect '/login'
-    end
-  end
-
-  get '/logout' do
-    if logged_in?
-      session.clear
-      redirect '/login'
-    else
-      redirect '/'
-    end
+  get '/users/:slug' do
+    # binding.pry
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
   end
 
 end
