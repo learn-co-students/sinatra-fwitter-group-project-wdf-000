8da8cecacb88f70e27b05d3d0ebafbed1d691bde
fidelscodes <fidelscodes@gmail.com>
2016-08-19 12:33:01 -0400

    Prevent signup if any field is empty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,35 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
+  # HOMEPAGE actions
   get '/' do
     erb :index
+  end
+
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  # Receive params from create_user view
+  # Redirect to /tweets upon successful creation
+  post '/signup' do
+    params_has_empty_value = params.values.any? {|val| val.empty? || val.nil?}
+    if params_has_empty_value
+      redirect to "/signup"
+    end
+
+    @user = User.create(params)
+    session[:user_id] = @user.id
+    redirect to "/tweets"
+  end
+
+  # Display all created tweets
+  get '/tweets' do
+    erb :'/tweets/tweets'
   end
 
 end
