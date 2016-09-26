a9daf84e3cc91e7f6c2706188d25f15b5c2c7c29
Kelene Chung <simoy93@gmail.com>
2016-09-26 01:31:34 -0400

    modified application controller and added main files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+	enable :sessions
+
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do 
+  	erb :'../index'
+  end
+
+  get '/signup' do 
+  	erb :'/users/signup'
   end
 
 end
